dacea5992c9260da90ccd8f8eab0a26882dd83ad
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2023-05-25 12:04:45 -0300

    Database seeding

--- backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
+++ backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.systemautoparts.settings.entities.TaxCode;
 import com.systemautoparts.settings.repositories.TaxCodeRepository;
@@ -13,7 +14,8 @@ public class TaxCodeService {
 
 	@Autowired
 	private TaxCodeRepository repository;
-	
+
+	@Transactional(readOnly = true)
 	public List<TaxCode> findAll() {
 		return repository.findAll();
 	}		
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category(name) VALUES ('Books')
+INSERT INTO tb_category(name) VALUES ('Electronics')
+INSERT INTO tb_category(name) VALUES ('Computers')
+
+INSERT INTO tb_taxcode(CFOP, description, application) VALUES ('5405', 'Merchandise Sales' , 'Sales of goods acquired or received from third parties in an operation with goods subject to the tax substitution regime, as a substituted taxpayer')
+INSERT INTO tb_taxcode(CFOP, description, application) VALUES ('6404', 'Sale out of State' , 'Sales of goods subject to the tax substitution regime, as a tax substitute, exclusively in cases where the tax has already been previously withheld')
+INSERT INTO tb_taxcode(CFOP, description, application) VALUES ('5411', 'Return of Goods' , 'Returns of goods purchased to be sold, whose entries have been classified as “Purchase for sale in an operation with goods subject to the tax substitution regime')
