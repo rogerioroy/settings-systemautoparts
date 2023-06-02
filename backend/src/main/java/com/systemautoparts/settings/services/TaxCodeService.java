4e4b47dff16271e30a51d6640dba3085b766c16a
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2023-06-02 12:47:04 -0300

    DELETE Category, Tax Code

--- backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
+++ backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.systemautoparts.settings.dto.TaxCodeDTO;
 import com.systemautoparts.settings.entities.TaxCode;
 import com.systemautoparts.settings.repositories.TaxCodeRepository;
+import com.systemautoparts.settings.services.exceptions.DatabaseException;
 import com.systemautoparts.settings.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -56,6 +59,18 @@ public class TaxCodeService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}	
 }
