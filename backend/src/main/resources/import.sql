77b5c0b18a8590e58599803168d1452aad2ce2b6
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2023-06-20 13:08:20 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_category(name) VALUES ('Books')
-INSERT INTO tb_category(name) VALUES ('Electronics')
-INSERT INTO tb_category(name) VALUES ('Computers')
+INSERT INTO tb_category(name, created_At) VALUES ('Books', NOW())
+INSERT INTO tb_category(name, created_At) VALUES ('Electronics', NOW())
+INSERT INTO tb_category(name, created_At) VALUES ('Computers', NOW())
 
 INSERT INTO tb_taxcode(CFOP, description, application) VALUES ('5405', 'Merchandise Sales' , 'Sales of goods acquired or received from third parties in an operation with goods subject to the tax substitution regime, as a substituted taxpayer')
 INSERT INTO tb_taxcode(CFOP, description, application) VALUES ('6404', 'Sale out of State' , 'Sales of goods subject to the tax substitution regime, as a tax substitute, exclusively in cases where the tax has already been previously withheld')
