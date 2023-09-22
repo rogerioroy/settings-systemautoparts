92548a636dcea3e168d72d676e7a9071910383cb
Rogerio Vasconcelos <rogerio_roy@hotmail.com>
2023-09-22 13:08:47 -0300

    Refactor pageable

--- backend/src/main/java/com/systemautoparts/settings/services/ClientService.java
+++ backend/src/main/java/com/systemautoparts/settings/services/ClientService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +25,8 @@ public class ClientService {
 	private ClientRepository clientRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Client> list = clientRepository.findAll(pageRequest);
+	public Page<ClientDTO> findAllPaged(Pageable pageable) {
+		Page<Client> list = clientRepository.findAll(pageable);
 		return list.map(x -> new ClientDTO(x));
 	}
 	
--- backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
+++ backend/src/main/java/com/systemautoparts/settings/services/TaxCodeService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +25,8 @@ public class TaxCodeService {
 	private TaxCodeRepository repository;
 
 	@Transactional(readOnly = true)
-	public Page<TaxCodeDTO> findAllPaged(PageRequest pageRequest) {
-		Page<TaxCode> list = repository.findAll(pageRequest);
+	public Page<TaxCodeDTO> findAllPaged(Pageable pageable) {
+		Page<TaxCode> list = repository.findAll(pageable);
 		return list.map(x -> new TaxCodeDTO(x));
 	}
 	
